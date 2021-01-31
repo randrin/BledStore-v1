3b25a4c4c83533b07a2cda53f8549542628d1b11
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-31 19:22:55 +0100

    push server backend frontend

--- backend/server.js
+++ backend/server.js
@@ -54,9 +54,14 @@ app.use((err, req, res, next) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get("/", (req, res) => {
-  res.send("Server is running ....");
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running ....");
+// });
 
 app.listen(config.PORT, () => {
   console.log(`Server started at http://localhost:${config.PORT}`);
