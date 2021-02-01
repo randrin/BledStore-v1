e981057b0384aa2999c12d0fe03f873b06b53a6c
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-02-01 19:32:11 +0100

    Fixed constants cart

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM_FAIL } from "../constants/cartConstants";
+export const CART_ADD_ITEM_FAIL = "CART_ADD_ITEM_FAIL";
 export const CART_ADD_ITEM = "CART_ADD_ITEM";
 export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
 
--- frontend/src/redux/reducers/cartsReducer.js
+++ frontend/src/redux/reducers/cartsReducer.js
@@ -1,5 +1,4 @@
-import { CART_ADD_ITEM_FAIL } from "../constants/cartConstants";
-
+export const CART_ADD_ITEM_FAIL = "CART_ADD_ITEM_FAIL";
 export const CART_ADD_ITEM = "CART_ADD_ITEM";
 export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
 export const CART_EMPTY_ITEM = "CART_EMPTY_ITEM";
